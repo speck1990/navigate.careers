3a996a8b6c28f5826c3ff202d99b4787c7845629
Stephen Peck <speck1990@gmail.com>
2019-08-31 12:04:35 -0500

    Add framework for htmlSerializer

--- src/helpers/prismicdom.js
+++ src/helpers/prismicdom.js
@@ -7,5 +7,68 @@ hbs.registerHelper("PrismicText", data => {
 });
 
 hbs.registerHelper("PrismicHtml", data => {
-	return PrismicDOM.RichText.asHtml(data, PrismicConfig.linkResolver);
+	const Elements = PrismicDOM.RichText.Elements;
+
+	const htmlSerializer = (type, element, content, children) => {
+		switch (type) {
+			// case Elements.heading1:
+			// 	return ``;
+
+			// case Elements.heading2:
+			// 	return ``;
+
+			// case Elements.heading3:
+			// 	return ``;
+
+			// case Elements.heading4:
+			// 	return ``;
+
+			// case Elements.heading5:
+			// 	return ``;
+
+			// case Elements.heading6:
+			// 	return ``;
+
+			// case Elements.paragraph:
+			// 	return ``;
+
+			// case Elements.preformatted:
+			// 	return ``;
+
+			// case Elements.strong:
+			// 	return ``;
+
+			// case Elements.em:
+			// 	return ``;
+
+			// case Elements.listItem:
+			// 	return ``;
+
+			// case Elements.oListItem:
+			// 	return ``;
+
+			// case Elements.list:
+			// 	return ``;
+
+			// case Elements.image:
+			// 	return ``;
+
+			// case Elements.embed:
+			// 	return ``;
+
+			// case Elements.hyperlink:
+			// 	return ``;
+
+			// case Elements.label:
+			// 	return ``;
+
+			// case Elements.span:
+			// 	return ``;
+
+			default:
+				return null;
+		}
+	};
+
+	return PrismicDOM.RichText.asHtml(data, PrismicConfig.linkResolver, htmlSerializer);
 });
