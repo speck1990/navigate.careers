54a5e21555949e2ae270c427a9cfc3b9e4ab1807
Stephen Peck <speck1990@gmail.com>
2019-09-13 00:06:25 -0500

    Remove dummy variables

--- src/routes/library.js
+++ src/routes/library.js
@@ -5,7 +5,7 @@ const PrismicInitApi = require("../utils/prismic-init");
 
 /* GET library page. */
 router.get("/library", async (req, res, next) => {
-	res.render("library", { i: "Dummy variable", j: "Another dummy variable" });
+	res.render("library", {});
 });
 
 module.exports = router;
