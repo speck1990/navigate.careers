e99122cf8fecb41cc1a63de28f88611f18551a80
Stephen Peck <speck1990@gmail.com>
2019-09-13 00:00:40 -0500

    Add another dummy variable and remove banner

--- src/routes/library.js
+++ src/routes/library.js
@@ -5,7 +5,7 @@ const PrismicInitApi = require("../utils/prismic-init");
 
 /* GET library page. */
 router.get("/library", async (req, res, next) => {
-	res.render("library", { i: "Dummy variable" });
+	res.render("library", { i: "Dummy variable", j: "Another dummy variable" });
 });
 
 module.exports = router;
