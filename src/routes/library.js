c25347092543943079ff18a562b2b1072122cdb2
Stephen Peck <speck1990@gmail.com>
2019-09-18 23:25:42 -0500

    Setup library data to be transferred to page
     - Connected to Prismic api
     - Created an array that contains all the categories that are
       used by articles for use in the filter on the page
     - Added category array and articles array to page render

--- src/routes/library.js
+++ src/routes/library.js
@@ -1,11 +1,37 @@
 const express = require("express");
 const router = express.Router();
 
+const Prismic = require("prismic-javascript");
 const PrismicInitApi = require("../utils/prismic-init");
 
 /* GET library page. */
 router.get("/library", async (req, res, next) => {
-	res.render("library", {});
+	try {
+		const api = await PrismicInitApi(req);
+		const response = await api.getSingle("library");
+
+		const articles = await api.query(Prismic.Predicates.at("document.type", "article"));
+
+		// Add category slugs to array
+		let usedCategories = articles.results.map(data => data.data.category.slug);
+		// Remove duplicates
+		usedCategories = [...new Set(usedCategories)];
+
+		const categoryType = await api.query(Prismic.Predicates.at("document.type", "category"));
+
+		const categories = []; // will contain the categories that are transferred to page
+		categoryType.results.forEach(category => {
+			usedCategories.find(usedCategory => {
+				if (usedCategory === category.slugs[0]) {
+					categories.push({ id: category.id, slug: category.slugs[0], name: category.data.name[0].text });
+				}
+			});
+		});
+
+		res.render("library", { document: response, categories, articles: articles.results });
+	} catch (error) {
+		res.status(404).send(error);
+	}
 });
 
 module.exports = router;
