94dbf0585b25e286ddc63cf5d11008237de20a08
Stephen Peck <speck1990@gmail.com>
2019-09-21 11:51:10 -0500

    Setup app to use article router

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const siteGlobals = require("./middleware/site-globals");
 // import routers
 const indexRouter = require("./routes/index");
 const libraryRouter = require("./routes/library");
+const articleRouter = require("./routes/article");
 const pageRouter = require("./routes/page");
 
 const app = express();
@@ -39,6 +40,7 @@ app.use(siteGlobals);
 // router setup
 app.use(indexRouter);
 app.use(libraryRouter);
+app.use(articleRouter);
 app.use(pageRouter);
 
 // catch 404 and forward to error handler
