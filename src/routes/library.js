ad62bcd813cfcef6856ead43c52bb766d2859259
Stephen Peck <speck1990@gmail.com>
2019-09-30 20:57:24 -0500

    Implement pagination for library page

--- src/routes/library.js
+++ src/routes/library.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const pagination = require("pagination");
 
 const Prismic = require("prismic-javascript");
 const PrismicInitApi = require("../utils/prismic-init");
 
 /* GET library page. */
 router.get("/library", async (req, res, next) => {
+	const page = req.query.page;
+
 	try {
 		const api = await PrismicInitApi(req);
 		const response = await api.getSingle("library");
 
-		const articles = await api.query(Prismic.Predicates.at("document.type", "article"), { orderings: "[document.first_publication_date desc]" });
+		const articles = await api.query(Prismic.Predicates.at("document.type", "article"), { orderings: "[document.first_publication_date desc]", pageSize: 2, page: page });
 
 		// Add category slugs to array
 		let usedCategories = articles.results.map(data => data.data.category.slug);
@@ -38,8 +41,40 @@ router.get("/library", async (req, res, next) => {
 			}
 		});
 
-		//console.log(articles.results);
-		res.render("library", { document: response, categories, articles: articles.results, noContainer: true });
+		let paginator = new pagination.TemplatePaginator({
+			prelink: "/library",
+			current: articles.page,
+			rowsPerPage: articles.results_per_page,
+			totalResult: articles.total_results_size,
+			template: function(result) {
+				let i, len, prelink;
+				let html = `<ul class="paginationz top-margin-70">`;
+				if (result.pageCount < 2) {
+					html += "</ul>";
+					return html;
+				}
+				prelink = this.preparePreLink(result.prelink);
+				if (result.previous) {
+					html += `<li class="prev"><a href="${prelink}${result.previous}"><i class="fa fa fa-caret-left"></i></a></li>`;
+				}
+				if (result.range.length) {
+					for (i = 0, len = result.range.length; i < len; i++) {
+						if (result.range[i] === result.current) {
+							html += `<li><a class="active" href="${prelink}${result.range[i]}" title="">${result.range[i]}</a></li>`;
+						} else {
+							html += `<li><a class="" href="${prelink}${result.range[i]}" title="">${result.range[i]}</a></li>`;
+						}
+					}
+				}
+				if (result.next) {
+					html += `<li class="prev"><a href="${prelink}${result.next}"><i class="fa fa fa-caret-right"></i></a></li>`;
+				}
+				html += "</ul>";
+				return html;
+			}
+		});
+
+		res.render("library", { document: response, categories, articles: articles, paginator: paginator.render(), noContainer: true });
 	} catch (error) {
 		res.status(404).send(error);
 	}
