e76d30f09eef6ed5975233e7fcc4c949e22aaa3c
Stephen Peck <speck1990@gmail.com>
2019-11-22 21:34:18 -0600

    Setup environment variables  - Install dotenv package  - Require dotenv and initialize  - Change session secret to env variable

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
@@ -47,7 +49,7 @@ app.use(express.static(path.join(__dirname, "../public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
 	expressSession({
-		secret: "compass careers will help you learn",
+		secret: process.env.SESSION_SECRET,
 		resave: false,
 		saveUninitialized: false
 	})
