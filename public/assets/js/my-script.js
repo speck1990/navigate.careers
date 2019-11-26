f67346f81295ba19e98e369ffb566a79b2ccf362
Stephen Peck <speck1990@gmail.com>
2019-11-25 19:02:19 -0600

    Add frontend code for the register form

--- public/assets/js/my-script.js
+++ public/assets/js/my-script.js
@@ -14,4 +14,30 @@ $(function() {
 			}
 		});
 	});
+
+	$("#registerForm").on("submit", event => {
+		event.preventDefault();
+		$(".loading").show();
+
+		const data = $(event.target).serialize();
+
+		$.post("/register", data, result => {
+			if (result.valid == true) {
+				window.location.href = "/library";
+			} else {
+				$(".loading").hide();
+				//$("#registerErrors").html(errors.email.message);
+				result.fields.forEach(field => {
+					const inputField = event.target[field];
+					if (result.errors.includes(field)) {
+						event.target[field].classList.add("is-invalid");
+						$("#registerErrors").html("Please complete required fields.");
+					} else {
+						event.target[field].classList.remove("is-invalid");
+						// event.target[field].classList.add("is-valid");
+					}
+				});
+			}
+		});
+	});
 });
