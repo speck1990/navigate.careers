0ade9e521f6f183a4462cb04ab58b37a00c856c2
Stephen Peck <speck1990@gmail.com>
2019-11-25 19:01:46 -0600

    Update register routes  - Removed "auth" middleware from get and post routes  - Add basic error handling for required fields  - Add other fields to register method  - Change what is returned after user is authenticated

--- src/routes/register.js
+++ src/routes/register.js
@@ -7,19 +7,33 @@ const PrismicInitApi = require("../utils/prismic-init");
 
 const auth = require("../middleware/auth");
 
-router.get("/register", auth, async (req, res) => {
+router.get("/register", async (req, res) => {
 	res.render("register", { layout: "layouts/simple" });
 });
 
-router.post("/register", auth, async (req, res) => {
-	User.register(new User({ username: req.body.username }), req.body.password, (err, user) => {
+router.post("/register", async (req, res) => {
+	const entries = Object.entries(req.body);
+	const fields = Object.keys(req.body);
+
+	let errors = [];
+	entries.forEach(entry => {
+		if (!entry[1]) {
+			errors.push(entry[0]);
+		}
+	});
+
+	if (errors.length > 0) {
+		return res.send({ fields, errors });
+	}
+
+	User.register(new User({ email: req.body.email, firstname: req.body.firstname, lastname: req.body.lastname }), req.body.password, (err, user) => {
 		if (err) {
 			console.log(err);
-			return res.render("register");
+			return res.send(err);
 		}
 
 		passport.authenticate("local")(req, res, () => {
-			res.redirect("/");
+			return res.send({ valid: true });
 		});
 	});
 });
