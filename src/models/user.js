ffd8f0d79ed3307e8661376a1d3d3676c90561be
Stephen Peck <speck1990@gmail.com>
2019-11-25 18:56:42 -0600

    Update schema with additional fields and properties

--- src/models/user.js
+++ src/models/user.js
@@ -1,11 +1,48 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 const passportLocalMongoose = require("passport-local-mongoose");
 
 const UserSchema = new mongoose.Schema({
-	username: String,
-	password: String
+	email: {
+		type: String,
+		unique: true,
+		required: true,
+		trim: true,
+		lowercase: true,
+		validate(value) {
+			if (!validator.isEmail(value)) {
+				throw new Error("Email is invalid");
+			}
+		}
+	},
+	password: {
+		type: String,
+		// required: true,
+		trim: true,
+		minlength: 7,
+		validate(value) {
+			if (value.toLowerCase().includes("password")) {
+				throw new Error('Password cannot contain "password"');
+			}
+		}
+	},
+	firstname: {
+		type: String,
+		trim: true
+	},
+	lastname: {
+		type: String,
+		trim: true
+	},
+	organization: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: false,
+		ref: "Organization"
+	}
 });
 
-UserSchema.plugin(passportLocalMongoose);
+UserSchema.plugin(passportLocalMongoose, {
+	usernameField: "email"
+});
 
 module.exports = mongoose.model("User", UserSchema);
