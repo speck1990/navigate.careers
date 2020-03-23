e0f9ed687ba00ed8775b6c65ede95376e7227d58
Stephen Peck <speck1990@gmail.com>
2020-03-23 15:21:00 -0500

    Create a condition if user is inactive to show unverified-email page

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,9 @@
 const auth = (req, res, next) => {
 	if (req.isAuthenticated()) {
+		if (!req.user.active) {
+			return res.render("unverified-email", { layout: "layouts/simple", email: req.user.email, userId: req.user._id });
+		}
+
 		return next();
 	}
 
