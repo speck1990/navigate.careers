e9e52418bc71b9e06edb54c467bdbe31f6671f58
Stephen Peck <speck1990@gmail.com>
2020-04-20 21:49:32 -0500

    Add timestamps fields

--- src/models/organization.js
+++ src/models/organization.js
@@ -1,24 +1,27 @@
 const mongoose = require("mongoose");
 
-const OrganizationSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-		trim: true
+const OrganizationSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			trim: true
+		},
+		domain: {
+			type: String,
+			required: true,
+			trim: true
+		},
+		accessCode: {
+			type: String,
+			unique: true,
+			required: true,
+			trim: true,
+			minlength: 7
+		}
 	},
-	domain: {
-		type: String,
-		required: true,
-		trim: true
-	},
-	accessCode: {
-		type: String,
-		unique: true,
-		required: true,
-		trim: true,
-		minlength: 7
-	}
-});
+	{ timestamps: true }
+);
 
 OrganizationSchema.virtual("users", {
 	ref: "User",
--- src/models/user.js
+++ src/models/user.js
@@ -3,40 +3,43 @@ const validator = require("validator");
 const crypto = require("crypto");
 const passportLocalMongoose = require("passport-local-mongoose");
 
-const UserSchema = new mongoose.Schema({
-	email: {
-		type: String,
-		unique: true,
-		required: true,
-		trim: true,
-		lowercase: true
+const UserSchema = new mongoose.Schema(
+	{
+		email: {
+			type: String,
+			unique: true,
+			required: true,
+			trim: true,
+			lowercase: true
+		},
+		firstname: {
+			type: String,
+			trim: true
+		},
+		lastname: {
+			type: String,
+			trim: true
+		},
+		organization: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			ref: "Organization"
+		},
+		secretToken: {
+			type: String
+		},
+		resetPasswordToken: {
+			type: String
+		},
+		resetPasswordExpires: {
+			type: Date
+		},
+		active: {
+			type: Boolean
+		}
 	},
-	firstname: {
-		type: String,
-		trim: true
-	},
-	lastname: {
-		type: String,
-		trim: true
-	},
-	organization: {
-		type: mongoose.Schema.Types.ObjectId,
-		required: true,
-		ref: "Organization"
-	},
-	secretToken: {
-		type: String
-	},
-	resetPasswordToken: {
-		type: String
-	},
-	resetPasswordExpires: {
-		type: Date
-	},
-	active: {
-		type: Boolean
-	}
-});
+	{ timestamps: true }
+);
 
 UserSchema.plugin(passportLocalMongoose, {
 	usernameField: "email"
